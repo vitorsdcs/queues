1400f719afe4c0a26b9bd9b5e9a101b40afc055e
vitorsdcs <vitorsdcs@outlook.com>
2018-01-20 17:22:20 -0200

    Testing json reading, filtering and printing with the sample input

--- src/queues/core.clj
+++ src/queues/core.clj
@@ -1,6 +1,29 @@
-(ns queues.core)
+(ns queues.core
+  (:require [cheshire.core :refer :all]))
 
-(defn -main
-  "I can say 'Hello World'."
-  []
-  (println "Hello, World!"))
+(def input-json
+  (map first (parse-stream (clojure.java.io/reader "sample-input.json") true)))
+
+(def agents
+  (->> input-json
+    (filter (fn [[k v]] (= k :new_agent)))
+    (map second)))
+
+(def jobs
+  (->> input-json
+    (filter (fn [[k v]] (= k :new_job)))
+    (map second)
+    (sort-by :urgent #(compare %2 %1))))
+
+(def job-requests
+  (->> input-json
+    (filter (fn [[k v]] (= k :job_request)))
+    (map second)))
+
+(defn -main []
+  (println "Agents")
+  (doseq [agent agents] (println agent))
+  (println "Jobs")
+  (doseq [job jobs] (println job))
+  (println "Job Requests")
+  (doseq [job-request job-requests] (println job-request)))
