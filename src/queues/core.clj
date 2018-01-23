875dcb04dbc824f61101f805bfcba045a98100b6
vitorsdcs <vitorsdcs@outlook.com>
2018-01-22 22:38:27 -0200

    Code organization

--- src/queues/core.clj
+++ src/queues/core.clj
@@ -1,23 +1,8 @@
 (ns queues.core
   (:require [cheshire.core :refer :all])
   (:require [queues.repository :refer :all]
+            [queues.handler :refer :all]
             [queues.exceptions :as ex]))
-
-(defn find-job [agent]
-  (or (get-highest-priority-job-by-skillset (agent :primary_skillset))
-      (get-highest-priority-job-by-skillset (agent :secondary_skillset))))
-
-(defn assign-job [job agent]
-  (save-job (conj job {:agent_id (agent :id)}))
-  (save-assignment job agent))
-
-(defn handle-job-request [job-request]
-  (let [agent (get-agent (job-request :agent_id))]
-    (if (some? agent)
-      (let [job (find-job agent)]
-        (if (some? job)
-          (assign-job job agent)))
-      (ex/no-agent-found (job-request :agent_id)))))
 
 (defn load-events [input]
   (as-> input input
